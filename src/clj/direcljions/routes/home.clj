563355dd8ecbce992dfee19133cd023030d9784b
Michel Mansour <michel.mansour@gmail.com>
2018-10-27 15:12:18 -0400

    Add page for directions
    - Add a new HTML template for getting directions via a form
    - Add a route and handler for the directions action
    - Add a link to the new page in the header

--- src/clj/direcljions/routes/home.clj
+++ src/clj/direcljions/routes/home.clj
@@ -1,6 +1,6 @@
 (ns direcljions.routes.home
   (:require [direcljions.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
             [clojure.java.io :as io]))
 
@@ -11,7 +11,15 @@
 (defn about-page []
   (layout/render "about.html"))
 
+(defn directions-page []
+  (layout/render "directions.html"))
+
+(defn get-directions [{:keys [params]}]
+  (layout/render "directions.html" {:start (:start params), :dest (:dest params)}))
+
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+  (GET "/directions" [] (directions-page))
+  (POST "/directions" request (get-directions request)))
 
