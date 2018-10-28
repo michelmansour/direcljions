c7bdfc849692184818af82345000b6c1a63245d4
Michel Mansour <michel.mansour@gmail.com>
2018-10-28 13:52:55 -0400

    Add input validation
    Validate that the directions request includes both an origin and a
    destination, and display a friendly error message in case either one is
    missing.

--- src/clj/direcljions/routes/home.clj
+++ src/clj/direcljions/routes/home.clj
@@ -4,7 +4,8 @@
             [ring.util.http-response :as response]
             [clojure.java.io :as io]
             [clj-http.client :as client]
-            [cheshire.core :refer :all]))
+            [cheshire.core :refer :all]
+            [struct.core :as st]))
 
 (defn home-page []
   (response/found "/directions"))
@@ -12,8 +13,11 @@
 (defn about-page []
   (layout/render "about.html"))
 
-(defn directions-page []
-  (layout/render "directions.html"))
+(defn directions-page [{:keys [flash]}]
+  (layout/render
+   "directions.html"
+   (merge {:start (:start flash) :dest (:dest flash)}
+          (select-keys flash [:name :message :errors]))))
 
 (defn parse-google-maps-response [json]
   (->>
@@ -32,19 +36,30 @@
    dest
    api-key))
 
+(def route-req-schema
+  [[:start [st/required
+            :message "Please enter an origin"]]
+   [:dest [st/required
+           :message "Please enter a destination"]]])
+
+(defn validate-route-request [params]
+  (first (st/validate params route-req-schema)))
+
 (defn get-directions [{:keys [params]}]
-  (layout/render "directions.html" {:start (:start params)
-                                    :dest (:dest params)
-                                    :directions (parse-google-maps-response
-                                                 (client/get
-                                                  (build-maps-url
-                                                   (:start params)
-                                                   (:dest params)
-                                                   "API_KEY")))}))
+  (if-let [errors (validate-route-request params)]
+    (-> (response/found "directions")
+        (assoc :flash (assoc params :errors errors)))
+    (layout/render "directions.html" {:start (:start params)
+                                      :dest (:dest params)
+                                      :directions (parse-google-maps-response
+                                                   (client/get
+                                                    (build-maps-url
+                                                     (:start params)
+                                                     (:dest params)
+                                                     "API_KEY")))})))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
   (GET "/about" [] (about-page))
-  (GET "/directions" [] (directions-page))
+  (GET "/directions" request (directions-page request))
   (POST "/directions" request (get-directions request)))
-
